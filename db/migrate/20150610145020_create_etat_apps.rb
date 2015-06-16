9cc1c8e6e1ed20048096f466e1b22e5627d593ac
CHENJing88 <jchen@nsadmin.infra.univ-tours.fr>
2015-06-16 14:29:25 +0200

    migrate

--- db/migrate/20150610145020_create_etat_apps.rb
+++ db/migrate/20150610145020_create_etat_apps.rb
@@ -2,7 +2,7 @@ class CreateEtatApps < ActiveRecord::Migration
   def change
     create_table :etat_apps do |t|
       t.references :etat, :polymorhic => true #intiquer polymorhic ici, il va generer
-                                              #etat_id et etat_type
+      t.string :nom                                        #etat_id et etat_type
       t.timestamps
     end
   end
