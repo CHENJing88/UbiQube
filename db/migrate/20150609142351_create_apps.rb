2c71b0cc31d1ddeec57781e6d0a3c5035d65f9db
JingCHEN <dianachenfr@gmail.com>
2015-06-16 12:30:16 +0200

    add des seeds sur "App" "Role" "User_App_Role"

--- db/migrate/20150609142351_create_apps.rb
+++ db/migrate/20150609142351_create_apps.rb
@@ -4,14 +4,14 @@ class CreateApps < ActiveRecord::Migration
       t.string :nom
       t.string :niveau
 
-      t.integer :etat_id
+      t.integer :etat_app_id
       t.integer :etape_id
       t.integer :groupe_app_id
 
       t.timestamps
     end
 
-    add_index :apps , :etat_id
+    add_index :apps , :etat_app_id
     add_index :apps , :etape_id
     add_index :apps , :groupe_app_id
   end
