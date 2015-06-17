139bccbc579beff52039e5dc2683c9acd8d1e2d1
CHENJing88 <jchen@nsadmin.infra.univ-tours.fr>
2015-06-17 08:46:32 +0200

    mes apps

--- app/models/user_app_role.rb
+++ app/models/user_app_role.rb
@@ -1,7 +1,7 @@
 class UserAppRole < ActiveRecord::Base
   #la relation many to many (user-app-role)
-  belongs_to :users , class_name: "User"#foreign key user_id
-  belongs_to :apps , class_name: "App" #foreign key app_id
-  belongs_to :roles , class_name: "Role"#foreign key role_id
+  belongs_to :users , class_name: "User", foreign_key:"user_id" #foreign key user_id
+  belongs_to :apps , class_name: "App",foreign_key:"app_id" #foreign key app_id
+  belongs_to :roles , class_name: "Role",foreign_key:"role_id" #foreign key role_id
 
 end
