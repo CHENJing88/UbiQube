ec066f924b31084554e9942e16e712cf6fcfb926
CHENJing88 <cjcara88@hotmail.com>
2015-06-23 14:11:03 +0200

    git commit -m "mesapps pages"

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,12 +1,10 @@
 class App < ActiveRecord::Base
   #les attributs modifable
- # attr_accessible :nom, :niveau, :etat_id, :etape_id, :groupe_app_id
+  #attr_accessible :nom, :niveau, :etat_id, :etape_id, :groupe_app_id
   #validation d'existance
-  #validates :nom, presence: true
-  #validates :niveau, presence: true
+  validates_presence_of :nom
 
   #relation d'APP
-
   #si une App est supprimée, on va supprimer tous les records dans user_app_roles
   has_many :user_app_roles, foreign_key: "app_id", dependent: :destroy
   #APP a plusieurs users et roles(many to many)
@@ -17,4 +15,5 @@ class App < ActiveRecord::Base
   has_many :etat_apps, :as => :etat_collection, :class_name => "EtatApp"
   belongs_to :groupe_app,:class_name => "GroupeApp", foreign_key:"groupe_app_id"
 
+  default_scope order: 'apps.created_at DESC'
 end
