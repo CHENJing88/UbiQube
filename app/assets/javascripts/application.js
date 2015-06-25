a70209c29d843ab5f63b471b5d634d890171f2a5
JingCHEN <dianachenfr@gmail.com>
2015-06-25 10:07:52 +0200

    js créer app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,24 +16,8 @@
 //= require turbolinks
 //= require foundation
 //= require_tree .
-$(document).ready(function(){
-    $('#create_app').click(function(e){
-      var url=$(this).attr('href');
-      var dialog_form=$('<div id="dialog-form">Loading form...</div>').dialog({
-            autoOpen: false,
-            width:520,
-            modal:true,
-            open: function(){
-              return $(this).load(url+' #content');
-            },
-            close: function(){
-              $('#dialog-form').remove();
-            }
-    });
-    dialog_form.dialog('open');
-    e.preventDefautlt();
-});
 
+window.Jquery = {};
 $(function() {
   $(document).foundation();
   });
--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,4 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 #appeller Created: <%= @my_object.created_at.to_s(:dots) %>
-[Time::DATE_FORMATS, Date::DATE_FORMATS].each do |obj| obj[:dots] = "%d/%m/%y"
+#[Time::DATE_FORMATS, Date::DATE_FORMATS].each do |obj| obj[:dots] = "%d/%m/%y"
