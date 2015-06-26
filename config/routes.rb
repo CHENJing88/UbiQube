ec2e8f07639aba9310a151594bf8baff36999e9b
JingCHEN <dianachenfr@gmail.com>
2015-06-26 11:24:55 +0200

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
 
 
-  get '/apps/new' => "apps#new", :as=>:new_app
+  get '/apps/new' => "apps#new"
   #get '/apps/:id/edit' => "apps#edit", :as=>:edit_app
   #patch '/apps/:id' => "apps#update"
 end
