5599276748c9154ea0217340a6a04ad39bd39bd5
JingCHEN <dianachenfr@gmail.com>
2015-06-30 15:54:24 +0200

    app form

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -4,4 +4,12 @@ module AppsHelper
     render :partial => app_path(app), :locals => { :app => app }, :controller=>"apps", :action=>"show", modal: true
   end
 
+  def app_valide?(app)
+    if app.etape_app_id > 2
+      return true
+    else
+      return false
+  end
+
+
 end
