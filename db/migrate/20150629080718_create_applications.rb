256532a76e6bd1aea3fd72257d15d96eb2ef7d1c
CHENJing88 <cjcara88@hotmail.com>
2015-06-30 12:42:14 +0200

    git commit -m "migrate"

--- db/migrate/20150629080718_create_applications.rb
+++ db/migrate/20150629080718_create_applications.rb
@@ -1,8 +1,17 @@
 class CreateApplications < ActiveRecord::Migration
   def change
     create_table :applications do |t|
+      t.string :nom
+      t.float :version
+      t.string :uid_admin
+      t.integer :etat_id
 
       t.timestamps
     end
+    add_index :applications , :etat_id
   end
+
+
+
+
 end
