4fc7d0bfcf7809225f4162c4110cde99c7eeca53
JingCHEN <dianachenfr@gmail.com>
2015-07-02 13:18:37 +0200

    ajax show app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,23 +19,14 @@
 $(function() {
   $(document).foundation();
   });
-$(document).ready(function(){
-    $('#show_app').click(function(e){
-      var url=$(this).attr('href');
-      var dialog_form=$('<div id="dialog-form">Loading form...</div>').dialog({
-            autoOpen: false,
-            width:520,
-            modal:true,
-            open: function(){
-              return $(this).load(url+' #content');
-            },
-            close: function(){
-              $('#dialog-form').remove();
-            }
-    });
-    dialog_form.dialog('open');
-    e.preventDefautlt();
-});
 
+$(document).ready(function(){
+  var clickOnPopupLink = function(){
+    $('body').on('click', '.static-popup-link', function(){
+      $('#show_app_modal').modal('show');
+    });
+  }
+
+  clickOnPopupLink();
 
 });
