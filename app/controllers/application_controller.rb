4478b7ce40b6226db54ac2a19711d1dba23ee381
JingCHEN <dianachenfr@gmail.com>
2015-07-07 12:59:18 +0200

    no message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,30 +41,5 @@ class ApplicationController < ActionController::Base
       end
     end
 
-    def ldap
-      require 'net/ldap'
-      @ldap = Net::LDAP.new :host => "ldap.univ-tours.fr",
-       :port => 389,
-       :auth => {
-             :method => :simple,
-             :username => "cn=rails_conn,ou=applis,dc=univ-tours,dc=fr",
-             :password => "s4cor;7Xvk2qSdrq"
-      }
-
-
-    end
-
-    def ldap_filtre(titre, var)
-      if ldap.bind
-        #filter = Net::LDAP::Filter.eq( "uid", current_user[:uid] )
-        #filter = Net::LDAP::Filter.eq( "UFRComposante", "DTIC" )
-        filter = Net::LDAP::Filter.eq( titre, var )
-        treebase = "ou=people,dc=univ-tours,dc=fr"
-        @results = ldap.search( :base => treebase, :filter => filter )
-        return @results
-      else
-          # authentication failed
-    	     logger.debug("authentication failed")
-      end
-    end
+    
 end
