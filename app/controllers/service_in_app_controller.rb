1b92e69c9647cbbdd9a8d74f081fde873bc3d616
JingCHEN <dianachenfr@gmail.com>
2015-07-09 11:54:05 +0200

    form select class

--- app/controllers/service_in_app_controller.rb
+++ app/controllers/service_in_app_controller.rb
@@ -1,2 +1,14 @@
 class ServiceInAppController < ApplicationController
+
+
+  def edit_tech
+    @service_in_app=ServiceInApp.find(params[:id])
+    @app=App.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.js{ render :layout => false }
+    end
+  end
+
 end
