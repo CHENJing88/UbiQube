90385a43e84d26dc817351d1832dc92e94057bf1
CHENJing88 <cjcara88@hotmail.com>
2015-07-15 11:17:56 +0200

    no message

--- app/models/service_in_app.rb
+++ app/models/service_in_app.rb
@@ -1,8 +1,8 @@
 class ServiceInApp < ActiveRecord::Base
-  has_many :reseau_services, foreign_key:"service_in_app_id"
+  has_many :reseau_services
   has_many :reseaus, through: :reseau_services
 
-  has_many :vm_install_service_ins, foreign_key:"service_in_app_id"
+  has_many :vm_install_service_ins
   has_many :vms, through: :vm_install_service_ins
 
   has_many :service_out_apps
