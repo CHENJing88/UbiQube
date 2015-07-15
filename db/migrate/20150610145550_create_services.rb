767eb13ad4716419aa8b3c27804433b69a840cfe
CHENJing88 <cjcara88@hotmail.com>
2015-07-15 11:27:48 +0200

    seeds

--- db/migrate/20150610145550_create_services.rb
+++ db/migrate/20150610145550_create_services.rb
@@ -4,7 +4,7 @@ class CreateServices < ActiveRecord::Migration
       t.string :nom
       t.integer :port_defaut
       t.string :protocole
-      t.boolean :is_defaut, :default => true
+      t.boolean :install_defaut, :defaut => true
       t.timestamps
     end
   end
