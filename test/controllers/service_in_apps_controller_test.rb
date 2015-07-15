a34239786915fa49cdb066b9d3a4dace46d88c42
CHENJing88 <cjcara88@hotmail.com>
2015-07-15 17:04:48 +0200

    test

--- test/controllers/service_in_apps_controller_test.rb
+++ test/controllers/service_in_apps_controller_test.rb
@@ -4,4 +4,9 @@ class ServiceInAppsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:apps)
+  end
 end
