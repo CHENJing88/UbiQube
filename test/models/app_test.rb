1f37299ca802c452bed8945f1a82f86de5c9a1d9
JingCHEN <dianachenfr@gmail.com>
2015-07-15 08:41:55 +0200

    test app

--- test/models/app_test.rb
+++ test/models/app_test.rb
@@ -4,4 +4,8 @@ class AppTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save app without title" do
+    post = Post.new
+    assert_not post.save, "Saved the app without a title"
+  end
 end
