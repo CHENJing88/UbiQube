576b755fd674f361e75d92248e762ba5c75834ce
CHENJing88 <cjcara88@hotmail.com>
2015-07-22 15:36:23 +0200

    no message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,15 +21,52 @@
   // require foundation $(document).foundation();
 
 $(document).ready(function() {
-        $('#VM-multiselect').multiselect({
-        enableClickableOptGroups: true,
-        includeSelectAllOption: true,
-        enableFiltering: true,
-        maxHeight: 200
-        });
-        $('#serv-multiselect').multiselect({
+      $('#rt-multiselect').multiselect({
+      enableClickableOptGroups: true,
+      includeSelectAllOption: true,
+      enableFiltering: true,
+      maxHeight: 200
+      });
+
+      $('#VM-multiselect').multiselect({
+      enableClickableOptGroups: true,
+      includeSelectAllOption: true,
+      enableFiltering: true,
+      maxHeight: 200
+      });
+
+      $('#serv-multiselect').multiselect({
         includeSelectAllOption: true,
         enableFiltering: true,
         maxHeight: 200
       });
+
+      $('#add_vm').on('click', function() {
+
+        var selected_vms = [];
+        $('#serv-multiselect option:selected').each(function() {
+            selected_vms.push($(this).val());
+        });
+
+        selected_vms.sort(function(a,b)){
+          return a[0]-b[0];
+        });
+
+        $.ajax({
+          url: '/posts/',
+          type: 'POST',
+          dataType: 'JSON',
+          data:{
+                title:$('#title_d').val(),
+                body:$('#body_d').val(),
+                author:$('#author_d').val()
+                }
+          success:function(data){
+            alert("dfdf");
+            $("#vm-table").append('<tr><td>data.title</td>
+                                    <td>data.body</td>
+                                    <td>data.author</td></tr>');
+          }
+        });
+
     });
