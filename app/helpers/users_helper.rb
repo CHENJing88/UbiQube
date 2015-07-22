e3ac4590afbf984c1e15db6387d313c39414357b
CHENJing88 <cjcara88@hotmail.com>
2015-07-22 16:38:25 +0200

    no message

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,12 @@
 module UsersHelper
 
   def required_role(need_role)
-  role = current_user.role
-  role_name = [role.nom]
+    role = current_user.role
+    role_name = [role.nom]
 
-  unless (need_role & role_name) == role_name
-    redirect_to root_path,:error=>"Non Permission"
+    unless (need_role & role_name) == role_name
+      redirect_to root_path,:error=>"Non Permission"
+    end
   end
 
 end
