427f3def2de13a27899dfde738cee00204a04507
CHENJing88 <cjcara88@hotmail.com>
2015-08-17 16:27:33 +0200

    no message

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,10 +36,10 @@ UserAppRole.create(user_id:1, app_id:1, role_id:1)
 ip1=Ip.create(ip_addr:167969652)#ip:10.3.3.116
 ip2=Ip.create(ip_addr:180552305)#ip:10.195.2.113
 
-vm1=Vm.create(nom:"Preprod", ip_defaut:180552305, template:"Windows7", etat_app_id:e1.id)
-vm2=Vm.create(nom:"Prod", ip_defaut:180552305, template:"Sabayon", etat_app_id:e1.id)
+vm1=Vm.create(nom:"Preprod", ip_default:180552305, template:"Windows7", etat_app_id:e1.id)
+vm2=Vm.create(nom:"Prod", ip_default:180552305, template:"Sabayon", etat_app_id:e1.id)
 
-s1=Service.create(nom:"SSH", port_defaut:22, protocole:"TCP", install_defaut:true)
+s1=Service.create(nom:"SSH", port_default:22, protocole:"TCP", install_default:true)
 
 sIN1=ServiceInApp.create(app_id:2, service_id:s1.id, port:s1.port_defaut)
 VmInstallServiceIn.create(service_in_app_id:sIN1.id, vm_id:vm1.id, ip_id:ip2.id)
