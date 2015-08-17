6a004536c9746b9217dcef258ef840ea84000284
CHENJing88 <cjcara88@hotmail.com>
2015-08-17 16:59:40 +0200

    no message

--- app/controllers/service_in_apps_controller.rb
+++ app/controllers/service_in_apps_controller.rb
@@ -5,7 +5,7 @@ before_action :set_app
     #@app=App.find(params[:app_id])
     @service_in_apps = @app.service_in_apps
     @service_in_apps.sort {|a,b| a.create_at.to_i <=> b.create_at.to_i}
-    @vm_installs = @service_in_apps.vms.build
+    @vm_installs = @service_in_app.vms.build
   end
 
   def show
