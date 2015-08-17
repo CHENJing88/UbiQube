ee23945ae80094a510bb0539baa8e93d0f7793ec
CHENJing88 <cjcara88@hotmail.com>
2015-08-17 16:23:50 +0200

    no message

--- db/migrate/20150610145020_create_etat_apps.rb
+++ db/migrate/20150610145020_create_etat_apps.rb
@@ -2,7 +2,7 @@ class CreateEtatApps < ActiveRecord::Migration
   def change
     create_table :etat_apps do |t|
       t.string :nom
-      t.references :etat_collection, :polymorphic=>true                       
+      #t.references :etat_collection, :polymorphic=>true                       
       t.timestamps
     end
   end
