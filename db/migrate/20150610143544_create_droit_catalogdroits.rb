4fbf0c327f713286f4ca473ab164ee00367f559d
CHENJing88 <cjcara88@hotmail.com>
2015-08-17 16:06:37 +0200

    no message

--- db/migrate/20150610143544_create_droit_catalogdroits.rb
+++ db/migrate/20150610143544_create_droit_catalogdroits.rb
@@ -1,12 +1,12 @@
 class CreateDroitCatalogdroits < ActiveRecord::Migration
   def change
     create_table :droit_catalogdroits do |t|
-      t.integer :role_id, index:true
+      t.integer :droit_id, index:true
       t.integer :catalogdroit_id, index:true
 
       t.timestamps
     end
 
-    add_index :droit_catalogdroits, [:role_id, :catalogdroit_id], unique:true
+    add_index :droit_catalogdroits, [:droit_id, :catalogdroit_id], unique:true
   end
 end
