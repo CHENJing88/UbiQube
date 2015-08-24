f26f27cdfc03856c1858ba6d7b5a4410308e386b
CHENJing88 <cjcara88@hotmail.com>
2015-08-24 16:48:31 +0200

    no message

--- test/models/vm_test.rb
+++ test/models/vm_test.rb
@@ -4,9 +4,9 @@ class VmTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "should not save vm without nom and description" do
+  test "should not save vm without nom" do
     vm = Vm.new
-    assert_not vm.save, "Saved the vm without a title and description"
+    assert_not vm.save, "Saved the vm without a title"
   end
 
 end
