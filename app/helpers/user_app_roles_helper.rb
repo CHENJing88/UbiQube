c4da7ec1afb96be9ad31582d73708eb7f8b2cf79
CHENJing88 <cjcara88@hotmail.com>
2015-08-25 17:06:19 +0200

    no message

--- app/helpers/user_app_roles_helper.rb
+++ app/helpers/user_app_roles_helper.rb
@@ -1,23 +1,29 @@
 module UserAppRolesHelper
-  def resp_fonc?(uars)
+  def resp_fonc?(uars, app)
+    @app=app
      #uar.role_id == 3 ? true : false
      uars.each do |uar|
        if uar.role_id == 3
          return uar
        end
      end
+     new_uar=UserAppRoles.create(role_id: 3, user_id: uars[1].user_id, app_id: uars[1].app_id)
+     @app.user_app_roles.build(new_uar)
 
-     return UserAppRoles.create(role_id: 3, user_id: uars[1].user_id, app_id: uars[1].app_id)
+     return new_uar
 
   end
 
-  def resp_tech?(uars)
+  def resp_tech?(uars, app)
+    @app=app
     uars.each do |uar|
       if uar.role_id == 4
         return uar
       end
     end
+    new_uar=UserAppRoles.create(role_id: 4, user_id: uars[1].user_id, app_id: uars[1].app_id)
+    @app.user_app_roles.build(new_uar)
 
-    return UserAppRoles.create(role_id: 4, user_id: uars[1].user_id, app_id: uars[1].app_id)
+    return new_uar
   end
 end
