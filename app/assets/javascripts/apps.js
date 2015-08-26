1aac0aba46fe777dc88cb577cd50c0c7d4436b1f
CHENJing88 <cjcara88@hotmail.com>
2015-08-26 15:07:52 +0200

    no message

--- app/assets/javascripts/apps.js
+++ app/assets/javascripts/apps.js
@@ -1,1 +1,14 @@
-
+$('#app_update').bind('toggle', function() {
+  $.ajax({
+    url: $(this).parents('form')[0].action,
+    headers: {
+      Accept : "text/javascript; charset=utf-8",
+      "Content-Type": 'application/x-www-form-urlencoded; charset=UTF-8'
+    },
+    type: 'PATCH',
+    data: {
+      'user_id': $(this).siblings('[id*=user_]').val(),
+      'authenticity_token': $(this).siblings('#authenticity_token').val()
+    }
+  });
+});
