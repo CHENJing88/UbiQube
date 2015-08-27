dafcd83e32daba5d64fbdcc573944e9c4ea583e9
CHENJing88 <cjcara88@hotmail.com>
2015-08-27 12:55:54 +0200

    no message

--- app/models/user_app_role.rb
+++ app/models/user_app_role.rb
@@ -1,9 +1,9 @@
 class UserAppRole < ActiveRecord::Base
-  #validates_presence_of :app_id, :role_id
+  validates_presence_of :app_id, :role_id
   #la relation many to many (user-app-role)
   belongs_to :users , class_name: "User", foreign_key:"user_id" #foreign key user_id
   belongs_to :apps , class_name: "App",foreign_key:"app_id" #foreign key app_id
   belongs_to :roles , class_name: "Role",foreign_key:"role_id" #foreign key role_id
 
-  #validates_uniqueness_of :user_id, scope: [:app_id, :role_id]
+  validates_uniqueness_of :user_id, scope: [:app_id, :role_id]
 end
