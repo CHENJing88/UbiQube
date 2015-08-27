6ba7e82e9e6d0935924a13eac1e1a429081e10fb
CHENJing88 <cjcara88@hotmail.com>
2015-08-27 17:28:07 +0200

    no message

--- app/controllers/user_app_roles_controller.rb
+++ app/controllers/user_app_roles_controller.rb
@@ -22,9 +22,9 @@ class UserAppRolesController < ApplicationController
   # GET /user_app_roles/new.xml
   def new
     @user_app_role = UserAppRole.new
-    @apps=@user_app_role.apps.build
-    @users=@user_app_role.users.build
-    @roles=@user_app_role.roles.build
+    @user_app_role.apps.build
+    @user_app_role.users.build
+    @user_app_role.roles.build
   end
   # GET /user_app_roles/1
   # GET /user_app_roles/1.xml
