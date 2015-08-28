70df0cc314e75bfe2748928097e9def4488dc56f
CHENJing88 <cjcara88@hotmail.com>
2015-08-28 14:24:51 +0200

    no message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require bootstrap-multiselect
 //= require_tree .
   // require foundation $(document).foundation();
