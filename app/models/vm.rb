08e739b0f597cce3dd1ed206f322175a32ef8e04
CHENJing88 <cjcara88@hotmail.com>
2015-08-31 14:38:41 +0200

    vm proxy

--- app/models/vm.rb
+++ app/models/vm.rb
@@ -1,3 +1,6 @@
 class Vm < ActiveRecord::Base
   validates_presence_of :nom
+  has_many :vm_install_service_ins
+  has_many :service_in_apps, through: :vm_install_service_ins
+  has_many :services, through: :vm_install_service_ins
 end
