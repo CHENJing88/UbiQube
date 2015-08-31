99b9624c66fb7246473807f45eab26daa0733c52
CHENJing88 <cjcara88@hotmail.com>
2015-08-31 15:52:38 +0200

    no message

--- app/controllers/service_in_apps_controller.rb
+++ app/controllers/service_in_apps_controller.rb
@@ -4,8 +4,8 @@ before_action :set_app
   def index
     @service_in_apps = @app.service_in_apps
     @service_in_apps.sort {|a,b| a.create_at.to_i <=> b.create_at.to_i}
-    @vm_installs=ServiceInApp.vm_install_service_ins.includes(:vms).uniq.plunk(:id)
-    @service_ins=ServiceInApp.services.uniq.plunk(:id)
+    @vm_installs=@app.service_in_apps.vm_install_service_ins.includes(:vms).uniq.plunk(:id)
+    @service_ins=@app.service_in_apps.services.uniq.plunk(:id)
   end
 
   def show
