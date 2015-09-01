23cb5b907dfec18e53bebce096cd7cd59035a17a
CHENJing88 <cjcara88@hotmail.com>
2015-09-01 15:25:27 +0200

    no message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,8 +10,6 @@ class SessionsController < ApplicationController
       if ldap_auth(auth['uid'])
         user = User.where(:provider => auth['provider'],
                           :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
-        user.email=auth.info.mail
-        user.name=auth['info']['displayname']
         reset_session
         session[:user_id] = user.id
         #redirect_to root_url, :notice => 'Signed in!'
