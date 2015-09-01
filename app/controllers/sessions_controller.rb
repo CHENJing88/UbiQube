0171f06385e8e5ea062e1336bcd86a0a37565ca2
CHENJing88 <cjcara88@hotmail.com>
2015-09-01 12:25:50 +0200

    no message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if ldap_filtre("UFRComposante", "DTIC") != nil
+    if ldap_filtre("UFRComposante", "DTIC")
       auth = request.env["omniauth.auth"]
       user = User.where(:provider => auth['provider'],
                         :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
