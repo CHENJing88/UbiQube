ae8ba3824da1c8586586a8e3b950101e098743f3
CHENJing88 <cjcara88@hotmail.com>
2015-09-01 14:13:07 +0200

    no message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
         session[:user_id] = user.id
         #redirect_to root_url, :notice => 'Signed in!'
         #redirect à la page de mes Apps
-        redirect_to mesapps_url(user.id), :notice => 'Signed in!'
+        redirect_to mesapps_url(user.id), :alert => 'Signed in!'
       #else
       #   flash.keep
       #   flash[:notice] ="Authentication error: que les personnelles du DTIC authorisées"
