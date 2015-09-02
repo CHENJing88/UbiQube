a6c135660ee35e7c59802cdc7404585890bd847c
CHENJing88 <cjcara88@hotmail.com>
2015-09-02 16:38:38 +0200

    new app

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -35,7 +35,7 @@ class AppsController < ApplicationController
   # GET /apps/new.xml
   def new
     @app = App.new
-    @user_app_roles=@app.user_app_roles.build
+    @app.user_app_roles.build
     #2.times { @app.user_app_roles.build }
     #@service_in_apps = @app.service_in_apps.build
   end
