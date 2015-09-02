115e88801befb595c152e94c2c34c5b69c01a25a
CHENJing88 <cjcara88@hotmail.com>
2015-09-02 11:38:15 +0200

    no message

--- app/models/app.rb
+++ app/models/app.rb
@@ -18,7 +18,7 @@ class App < ActiveRecord::Base
   belongs_to :groupe_app,:class_name => "GroupeApp", foreign_key:"groupe_app_id"
 
   #default_scope order: 'apps.created_at DESC'
-  accepts_nested_attributes_for :user_app_roles, allow_destroy: true, reject_if: :reject_uars
+  accepts_nested_attributes_for :user_app_roles, allow_destroy: true #, reject_if: :reject_uars
   accepts_nested_attributes_for  :service_in_apps, allow_destroy: true
 
   scope :envoie, -> { where(is_envoye: true) }
