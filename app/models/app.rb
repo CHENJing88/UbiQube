ea19f31d34cfe51058405f258e11057d3ad31e1c
CHENJing88 <cjcara88@hotmail.com>
2015-09-02 11:44:32 +0200

    no message

--- app/models/app.rb
+++ app/models/app.rb
@@ -4,7 +4,7 @@ class App < ActiveRecord::Base
 
   #relation d'APP
   #si une App est supprimée, on va supprimer tous les records dans user_app_roles
-  has_many :user_app_roles, foreign_key: "app_id", dependent: :destroy
+  has_many :user_app_roles, foreign_key: "app_id", dependent: :destroy, inverse_of: :app
   #APP a plusieurs users et roles(many to many)
   has_many :users, through: :user_app_roles
   has_many :roles, through: :user_app_roles
@@ -17,9 +17,8 @@ class App < ActiveRecord::Base
   belongs_to :etat_app, :class_name => "EtatApp", foreign_key:"etat_app_id"
   belongs_to :groupe_app,:class_name => "GroupeApp", foreign_key:"groupe_app_id"
 
-  #default_scope order: 'apps.created_at DESC'
-  accepts_nested_attributes_for :user_app_roles, allow_destroy: true #, reject_if: :reject_uars
-  accepts_nested_attributes_for  :service_in_apps, allow_destroy: true
+  accepts_nested_attributes_for :user_app_roles, allow_destroy: true , reject_if: :reject_uars
+  #accepts_nested_attributes_for  :service_in_apps, allow_destroy: true
 
   scope :envoie, -> { where(is_envoye: true) }
 
