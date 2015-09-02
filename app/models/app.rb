9e01630e591ac0a307d8da97df50de48ba9203b4
CHENJing88 <cjcara88@hotmail.com>
2015-09-02 14:21:20 +0200

    new app

--- app/models/app.rb
+++ app/models/app.rb
@@ -17,14 +17,14 @@ class App < ActiveRecord::Base
   belongs_to :etat_app, :class_name => "EtatApp", foreign_key:"etat_app_id"
   belongs_to :groupe_app,:class_name => "GroupeApp", foreign_key:"groupe_app_id"
 
-  #, allow_destroy: true 
-  accepts_nested_attributes_for :user_app_roles , reject_if: :reject_uars
+  #
+  accepts_nested_attributes_for :user_app_roles , allow_destroy: true , reject_if: :reject_uars
   #accepts_nested_attributes_for  :service_in_apps, allow_destroy: true
 
   scope :envoie, -> { where(is_envoye: true) }
 
   def reject_uars(attributed)
-    attributed['user_id'].blank? || attributed['app_id'].blank? || attributed['role_id'].blank?
+    attributed['user_id'].blank? || attributed['role_id'].blank?
   end
 
   # pour "accepts_nested_attributes_for" quand il faut transmettre des attributes d'autre model
