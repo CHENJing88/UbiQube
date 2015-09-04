e13fceabb55b7cb42fcea77536e837fad0986ac6
CHENJing88 <cjcara88@hotmail.com>
2015-09-04 15:00:44 +0200

    no message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,9 @@ class UsersController < ApplicationController
   def mesapps
     @user = User.find(session[:user_id])
     @mesapps=@user.user_app_roles.includes(:apps,:roles)
-    redirect_to mesapps_path(@user)
   end
   def admin
-    
+
     @apps=App.all
   end
   def dsi
