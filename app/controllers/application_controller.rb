e572a6d8ff34743e8e82569b226d97187c0c9b7c
CHENJing88 <cjcara88@hotmail.com>
2015-09-14 10:44:46 +0200

    no message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     render :file => "#{Rails.root}/public/403.html", :status => 403,:layout => false
     end
-  helper_method :current_user,:correct_user?
+  helper_method :current_user
+  helper_method :correct_user?
   helper_method :user_signed_in?
   #helper_method :current_ability
   helper_method :ldap
@@ -44,9 +45,9 @@ class ApplicationController < ActionController::Base
       end
     end
 
-    #def current_ability
-    #  @current_ability ||= ::Ability.new(current_user)
-    #end
+    def current_ability
+      @current_ability ||= ::Ability.new(current_user)
+    end
 
     def load_and_authorize_resource
      load_resource
