508eec7e52cbe383e64413f694b6ff1efa7e9d0b
CHENJing88 <cjcara88@hotmail.com>
2015-09-14 15:35:59 +0200

    no message

--- app/models/user_groupe_user.rb
+++ app/models/user_groupe_user.rb
@@ -1,2 +1,10 @@
 class UserGroupeUser < ActiveRecord::Base
+  validates_presence_of :user_id, :groupe_user_id
+  #before_validation do |model|
+    #model.user_id.reject!(&:blank?) if model.user_id
+  #end
+  #la relation many to many (user-app-role)
+  belongs_to :users , class_name: "User", foreign_key:"user_id" #foreign key user_id
+  belongs_to :groupe_user , class_name: "GroupeUser",foreign_key:"groupe_user_id"#foreign key app_id,
+  
 end
