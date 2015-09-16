f4ab5c8c29315aba708832b75e6c243734780955
CHENJing88 <cjcara88@hotmail.com>
2015-09-16 12:54:11 +0200

    no message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,6 +22,18 @@ module ApplicationHelper
     return true if current_user[:uid].to_s== Application.first.uid_dsi.to_s
   end
 
+  def ldap_auth(uid, local)
+    if ldap.bind
+      filter = Net::LDAP::Filter.eq( "uid", uid )
+      #filter = Net::LDAP::Filter.eq( "ufrcomposante", "DTIC" )
+      treebase = "ou=people,dc=univ-tours,dc=fr"
+      @results = ldap.search( :base => treebase, :filter => filter )
+      return @results[0].ufrcomposante[0].eql? local
+    else
+        # authentication failed
+         logger.debug("ldap authentication filter failed")
+    end
+  end
 
   # Limit le lecture d'info d'user
   def ldap_filtre(titre, var)
