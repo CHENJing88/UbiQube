4a6f20da68fd4a6ba576bcdb80da8b037f66e716
CHENJing88 <cjcara88@hotmail.com>
2015-09-28 12:19:27 +0200

    no message

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,19 +4,20 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-
+  setup do
+    @app= apps(:http)
+    @user=users(:jing)
+    @role=roles(:rt)
+  end
   test "assigned user_app_roles" do
-    user=User.create(assigns(:jing))
-    app=App.create(assigns(:http))
-    role=Role.create(assigns(:rt))
-    assert_empty user.user_app_roles
-    assert_empty app.user_app_roles
-    assert_empty role.user_app_roles
+    assert_empty @user.user_app_roles
+    assert_empty @app.user_app_roles
+    assert_empty @role.user_app_roles
 
-    user.user_app_roles.create(app_id:app.id, role_id: role.id)
+    @user.user_app_roles.create(app_id:app.id, role_id: role.id)
 
-    assert_equal 1, app.user_app_roles.size
-    assert_equal 1, role.user_app_roles.size
+    assert_equal 1, @app.user_app_roles.size
+    assert_equal 1, @role.user_app_roles.size
   end
 
 
