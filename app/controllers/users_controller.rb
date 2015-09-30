fbad7b08fac71eb814bd413f48e4f1b3e3291b4d
CHENJing88 <cjcara88@hotmail.com>
2015-09-30 14:48:23 +0200

    no message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!,:correct_user?
   load_resource
   def index
     #@users = User.all
